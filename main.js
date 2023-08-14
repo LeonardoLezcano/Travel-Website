768171ae312d744e4c01b857317fe8164a8fd221
Leonardo Lezcano <leonardo.d.lezcano@gmail.com>
2023-08-14 16:35:18 -0300

    Pagina completa

--- main.js
+++ main.js
@@ -3,3 +3,11 @@ window.addEventListener("scroll", function () {
   var nav = this.document.querySelector("nav");
   nav.classList.toggle("sticky", window.scroll > 0);
 });
+
+//Responsive Navbar
+function toggleMenu() {
+  var menuToggle = document.querySelector(".toggle");
+  var menu = document.querySelector(".menu");
+  menuToggle.classList.toggle("active");
+  menu.classList.toggle("active");
+}
